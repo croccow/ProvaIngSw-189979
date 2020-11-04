e09bac13fabaf37975204342ea7321cca528ffcb
Walter <crcwtr96m28086r@studenti.unical.it>
2020-11-04 12:31:36 +0100

    Esercizio svolto Rimane da fare i test

--- src/main/java/LabIngswProvaIntermedia/App.java
+++ src/main/java/LabIngswProvaIntermedia/App.java
@@ -1,5 +1,11 @@
 package LabIngswProvaIntermedia;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.joda.time.DateTime;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       MyListUtil ml = new MyListUtil();
+       List <Integer> l = new ArrayList <Integer>() ;
+      
+       l.add(13);
+       l.add(17);
+       l.add(19);
+       l.add(6);
+       l.add(5);
+       l.add(14);
+       Scanner in = new Scanner(System.in);
+       DateTime dt = new DateTime();
+       System.out.println( "Today is: " + dt.toString("yyyy-MM-dd") );
+       System.out.println("Scegli come vuoi ordinare la lista: 0 per ordinarla in modo crescente 1 per ordinarla in modo decrescente");
+       int scelta = in.nextInt();
+       ml.scegliLista(l, scelta);
+       
     }
 }
--- src/main/java/LabIngswProvaIntermedia/MyListUtil.java
+++ src/main/java/LabIngswProvaIntermedia/MyListUtil.java
@@ -1,5 +1,46 @@
 package LabIngswProvaIntermedia;
 
+
+import java.util.Collections;
+
+import java.util.List;
+
 public class MyListUtil {
+	
+	public String scegliLista(List<Integer> l, int scelta) {
+		if(l.isEmpty()) {
+			return "La lista è vuota";
+		}
+		
+		int cont =0;
+		if(scelta == 0 ) {
+			Collections.sort(l);
+			
+		}
+		else if(scelta == 1) {
+			Collections.sort(l,Collections.reverseOrder());
+			
+		}
+			
+		
+			
+			for(Integer i: l) {
+				System.out.println(i);
+			}
+		
+		return "La lista è stata ordinata in modo crescente ";
+	}
+	
+
+	
+	
+
+	
+
+	
+
+	
+	
+		
 	
 }
